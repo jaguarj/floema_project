837dcd820af3d57d4021618bfbd25cc2155b4490
Josh Seipel <joshseipel@gmail.com>
2021-12-13 09:13:55 -0500

    Updated webpack config files.

---
+++ webpack.config.build.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const IS_DEVELOPMENT = process.env.NODE_ENV === 'dev';
+
+const dirApp = path.join(__dirname, 'app');
+const dirAssests = path.join(__dirname, 'assets');
+const dirStyles = path.join(__dirname, 'styles');
+
+console.log(dirApp, dirAssests, dirStyles);
