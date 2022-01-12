76e787f7097bee8c6da1317fc5b3a140d14ffc50
Josh Seipel <joshseipel@gmail.com>
2022-01-12 12:16:52 -0500

    Updated ImageMinimizerPlugin schema.

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@ const webpack = require('webpack');
 
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { dir } = require('console');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+
 const IS_DEVELOPMENT = process.env.NODE_ENV === 'dev';
 
 const dirApp = path.join(__dirname, 'app');
@@ -45,7 +46,35 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: '[name].css',
             chunkFilename: '[id].css'
-        })
+        }),
+
+        // new ImageMinimizerPlugin({
+        //     minimizer: {
+        //         options: {
+        //             plugins: [
+        //                 ['gifsicle', { interlaced: true}],
+        //                 ['jpegtran', { progressive: true}],
+        //                 ['optipng', { optimizationLevel: 5}],
+        //             ],
+        //         },
+                
+        //     },
+        // }),
+
+        new ImageMinimizerPlugin({
+            minimizer: {
+              implementation: ImageMinimizerPlugin.imageminMinify,
+              options: {
+                // Lossless optimization with custom option
+                // Feel free to experiment with options for better result for you
+                plugins: [
+                  ["gifsicle", { interlaced: true }],
+                  ["jpegtran", { progressive: true }],
+                  ["optipng", { optimizationLevel: 5 }],
+                ],
+              },
+            },
+          }),
     ],
 
     module: {
@@ -81,9 +110,9 @@ module.exports = {
                 test: /\.(jpe?g|png|gif|svg|woff2?|fnt|webp)$/,
                 loader: 'file-loader',
                 options: {
-                    // name (file) {
-                    //     return '[hash].[ext]'
-                    // }
+                    name (file) {
+                        return '[name].[hash].[ext]'
+                    }
                 }
             }
         ]
