97ef02dc145e7d705fc2a15c487b1ffbeabff15b
Josh Seipel <joshseipel@gmail.com>
2022-02-08 11:25:16 -0500

    Added pages, renamed template to base and updated routes.

--- app.js
+++ app.js
@@ -5,15 +5,21 @@ const port = 3000
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
+
 app.get('/', (req, res) => {
-  res.render('template', {
-    meta: {
-      data: {
-        title: 'hello',
-        description: 'Project'
-      }
-    }
-  })
+  res.render('pages/home')
+})
+
+app.get('/about', (req, res) => {
+  res.render('pages/about')
+})
+
+app.get('/collections', (req, res) => {
+  res.render('pages/collections')
+})
+
+app.get('/detail/:id', (req, res) => {
+  res.render('pages/details')
 })
 
 app.listen(port, () => {
